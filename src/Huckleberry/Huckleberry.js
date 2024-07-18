b940ea13c1d481d539ad863bc541a5fb4a749e70
RidwanAdebosin <adebosinadewale@gmail.com>
2024-07-18 22:46:00 +0100

    Responsiveness

--- src/Huckleberry/Huckleberry.js
+++ src/Huckleberry/Huckleberry.js
@@ -2,34 +2,40 @@ import "./Huckleberry.css";
 
 const Huckleberry = () => {
     return (
-        <main>
-        <div className="container-wrapper">
-        <section className="first-section">
+        <>
+        <header>
+        <div className="max-width-wrapper">
+         <div className="intro-chunk">
+    
             <h1>Huckleberry</h1>
-            <div className="first-content">
             <p>An avant-garde agency focused on connecting <span>passion</span> with <span>logistics.</span></p>
             <p>A better way to solve the problems of tomorrow</p>
-            </div>
-           
-        </section>
-        <section className="second-section">
-            <article>
-            <h1>Wealth comes from operations</h1>
+        
+         </div>
+        </div> 
+        </header>  
+        <main>
+        <section className="max-width-wrapper">
+            <div className="card">
+            <h2 className="indented-heading">Wealth comes from operations</h2>
             <p>
                 No modern business can thrive without developing a specialization in operations. Huckleberry has helped dozens of companies, from Fortune 50 monoliths to fast-growing Bay Area startups, achieve their goals and meet their KPIs by leveraging our extensive network of logistics experts.
             </p>
             <p>
                 Huckleberry was established in 2003 to help ambitious business discover their true potential. If that resonates, let's chat!
             </p>
-            </article>
+
+            </div>
 
 
         </section>
+    </main>
+        
+        </>
 
-        </div>
 
 
-        </main>
+      
     )
 };
 
